e257c4c17e44b05ce62502cd3139ed4aa2392ada
Viktor286 <just.viktor@gmail.com>
2020-05-24 13:55:03 -0400

    SnapShot class and fixture set

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,11 @@
 // import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import * as PIXI from 'pixi.js'
+import * as PIXI from 'pixi.js';
+import {SnapShot, SnapShotLoader} from "./interfaces/SnapShot";
+import { Viewport } from 'pixi-viewport';
+import { urlSmallSet } from "./fixtures/imagesDataSet.ts";
+
 
 const app = new PIXI.Application({
   width: 1200,
@@ -15,18 +19,48 @@ const app = new PIXI.Application({
 const appDiv = document.querySelector('.app');
 if (appDiv) {
   appDiv.appendChild(app.view);
+
+  const viewport = new Viewport({
+    screenWidth: window.innerWidth,
+    screenHeight: window.innerHeight,
+    worldWidth: 1000,
+    worldHeight: 1000,
+
+    interaction: app.renderer.plugins.interaction // the interaction module is important for wheel to work properly when renderer.view is placed or scaled
+  });
+
+  // add the viewport to the stage
+  app.stage.addChild(viewport)
+
+  // activate plugins
+  viewport
+    .drag()
+    .pinch()
+    .wheel()
+    .decelerate();
+
+  const loader = new PIXI.Loader();
+  loader.add(urlSmallSet)
+    .on("progress", loadProgressHandler)
+    .load(setup);
+
+  let rectangle = new PIXI.Graphics();
+  rectangle.lineStyle(3, 0xFF3300, 1);
+  rectangle.drawRect(0, 0, 100, 100);
+  viewport.addChild(rectangle);
+
+  function loadProgressHandler(loader: PIXI.Loader, resource: PIXI.LoaderResource):void {
+    console.log("loading: " + resource.url);
+    // console.log("progress: " + loader.progress + "%");
+    //If you gave your files names as the first argument
+    //of the `add` method, you can access them like this
+    //console.log("loading: " + resource.name);
+  }
+
+  function setup(loader: PIXI.Loader) {
+    const snapShots = new SnapShotLoader(loader.resources);
+    snapShots.store.forEach((snapShot: SnapShot) => {
+      viewport.addChild(snapShot.snapShot.sprite);
+    });
+  }
 }
-
-// import * as serviceWorker from './serviceWorker';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
---
+++ src/interfaces/SnapShot.ts
@@ -0,0 +1,41 @@
+import * as PIXI from 'pixi.js';
+
+interface ISnapShotObject {
+  title: string,
+  resourceUrl: string,
+  sprite: PIXI.Sprite
+}
+
+interface ISnapShot extends PIXI.Container {
+  snapShot: ISnapShotObject
+}
+
+export class SnapShot extends PIXI.Container implements ISnapShot {
+  public snapShot: ISnapShotObject;
+
+  constructor(resourceUrl: string, texture: PIXI.Texture) {
+    super();
+
+    const image = PIXI.Sprite.from(texture);
+    this.snapShot = {
+      title: '',
+      sprite: image,
+      resourceUrl
+    };
+
+    this.addChild(image);
+  }
+}
+
+interface ISnapShotLoader {
+  store: Array<SnapShot>
+}
+
+export class SnapShotLoader implements ISnapShotLoader {
+  public store: Array<SnapShot> = [];
+  constructor(resources: PIXI.IResourceDictionary) {
+    for (const [resourceUrl, resource] of Object.entries(resources)) {
+      this.store.push(new SnapShot(resourceUrl, resource.texture));
+    }
+  }
+}
