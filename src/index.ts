bed8ec7a2fc525767db6f86f13b7d55c04e3d36d
Viktor286 <just.viktor@gmail.com>
2020-05-24 20:57:06 -0400

    create drawSelection

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import './index.css';
 import * as PIXI from 'pixi.js';
 import { SnapShot, SnapShotLoader } from './interfaces/SnapShot';
 import { Viewport } from 'pixi-viewport';
-import { urlSmallSet } from './fixtures/imagesDataSet';
+import { urlLargeSet } from './fixtures/imagesDataSet';
 import { SpaceModifiers } from "./interfaces/SpaceModifiers";
 
 const app = new PIXI.Application({
@@ -29,16 +29,38 @@ if (appDiv) {
   // add the viewport to the stage
   app.stage.addChild(viewport);
 
+  let rectangle = new PIXI.Graphics();
+  rectangle.lineStyle(1.1, 0xff3300, 1);
+  rectangle.drawRect(300, 300, 100, 100);
+  viewport.addChild(rectangle);
+
+  viewport.on('zoomed', e => {
+    const zoomLevel = e.viewport.transform.scale.x;
+    // console.log('e.viewport', e.viewport);
+
+    e.viewport.children.forEach(container => {
+      if (container instanceof SnapShot && container.snapShot.selected) {
+        container.drawSelection(zoomLevel);
+      }
+    })
+
+    // This might be not 100% correct
+    // const zoomCoords = {
+    //   level: e.viewport.transform.scale.x,
+    //   toPointX: e.viewport.transform.position.x,
+    //   toPointY: e.viewport.transform.position.y,
+    //   fromPointX: e.viewport.x,
+    //   fromPointY: e.viewport.y,
+    // };
+    // console.log('e', e);
+    // console.log('zoomCoords', zoomCoords);
+  })
+
   // activate plugins
   viewport.drag().pinch().wheel().decelerate();
 
   const loader = new PIXI.Loader();
-  loader.add(urlSmallSet).on('progress', loadProgressHandler).load(setup);
-
-  let rectangle = new PIXI.Graphics();
-  rectangle.lineStyle(3, 0xff3300, 1);
-  rectangle.drawRect(0, 0, 100, 100);
-  viewport.addChild(rectangle);
+  loader.add(urlLargeSet).on('progress', loadProgressHandler).load(setup);
 
   function loadProgressHandler(
     loader: PIXI.Loader,
@@ -53,7 +75,7 @@ if (appDiv) {
 
   function setup(loader: PIXI.Loader) {
     const snapShots = new SnapShotLoader(loader.resources);
-    SpaceModifiers.positionGrid(snapShots.store);
+    SpaceModifiers.positionGrid(snapShots.store, 8, 230, 120);
 
     snapShots.store.forEach((s, i) => {
       s.scale = new PIXI.Point(.1, .1);
--- src/interfaces/SnapShot.ts
+++ src/interfaces/SnapShot.ts
@@ -4,6 +4,10 @@ interface ISnapShotObject {
   title: string;
   resourceUrl: string;
   sprite: PIXI.Sprite;
+  selectionDrawing: PIXI.Graphics,
+  selected: boolean,
+  width: number,
+  height: number
 }
 
 interface ISnapShot extends PIXI.Container {
@@ -21,9 +25,22 @@ export class SnapShot extends PIXI.Container implements ISnapShot {
       title: '',
       sprite: image,
       resourceUrl,
+      selectionDrawing:  new PIXI.Graphics(),
+      selected: true,
+      width: image.width,
+      height: image.height
     };
 
     this.addChild(image);
+    this.addChild(this.snapShot.selectionDrawing);
+    // console.log('SnapShot', this);
+  }
+
+  drawSelection(zoomLevel: number = 1) {
+    this.snapShot.selectionDrawing
+      .clear()
+      .lineStyle(1.1 / zoomLevel / this.transform.scale.x, 0x73b2ff)
+      .drawRect(0, 0, this.snapShot.width, this.snapShot.height)
   }
 }
 
