383b91acb59b50661d8caa3b3202e5b8c1d95ba3
Viktor286 <just.viktor@gmail.com>
2020-09-22 19:07:04 -0400

    upd setPositionGridd

--- src/modifiers/SpaceModifiers.ts
+++ src/modifiers/SpaceModifiers.ts
@@ -1,33 +1,36 @@
-import { IPublicMemo } from '../Interfaces/Memos';
+import { Memo } from '../Interfaces/Memos';
 
 /** TODO: change modifier interface to matrix output (prev, next states for animation) **/
 export class SpaceModifiers {
   static setPositionGrid(
-    elements: IPublicMemo[],
+    elements: Map<string, Memo>,
     colLimit: number = 10,
     cellWidth: number = 300,
     cellHeight: number = 100,
     scale: number = 1,
   ) {
-    // let col = 0;
-    // let row = 0;
-    // elements.forEach((el, idx) => {
-    //   const x = (idx % colLimit) * cellWidth;
-    //   if (col >= colLimit) {
-    //     col = 0;
-    //     row++;
-    //   }
-    //   col++;
-    //   const y = row * cellHeight;
-    //
-    //   console.log('Apply transforms', x, y, scale);
-    //   // Apply transforms
-    //   // el.x = x;
-    //   // el.y = y;
-    //   // el.scale.x = scale;
-    //   // el.scale.y = scale;
-    //
-    //   idx++;
-    // });
+    let col = 0;
+    let row = 0;
+    let idx = 0;
+    elements.forEach((el) => {
+      const x = (idx % colLimit) * cellWidth;
+      if (col >= colLimit) {
+        col = 0;
+        row++;
+      }
+      col++;
+      const y = row * cellHeight;
+
+      // Apply transforms
+      // TODO: how would we access state here?
+      //  we should return array of mutation orders
+      //  instead of modifying origin here here
+
+      el.x = x;
+      el.y = y;
+      el.scale = scale;
+
+      idx++;
+    });
   }
 }
